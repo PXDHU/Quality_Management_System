c647b42402b565e0452913399bb56253da8393c7
Padmavasan Balakrishnan <padmavasan.contact@gmail.com>
2025-08-01 15:38:19 +0530

    first commit

---
+++ src/test/java/com/example/QualityManagementSystem/QualityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QualityManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
